6b0db42593bb1d8cea6b1a61e3b8d7c4bd616e8b
Felipe Farias <fefarias97@gmail.com>
2021-08-22 19:51:35 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeescola = "Etec Adolpho Berezin";
+            Console.WriteLine(nomeescola);
+        }
+    }
+}
